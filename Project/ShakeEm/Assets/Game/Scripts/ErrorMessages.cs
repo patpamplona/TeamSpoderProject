2eeb0576923f369e4f6b76a47c7d859b30897789
Patrick Karlo Pamplona <patrick.pam@pocketplaylab.com>
2015-05-21 09:56:58 +0800

    eror messages

---
+++ Project/ShakeEm/Assets/Game/Scripts/ErrorMessages.cs
@@ -0,0 +1,7 @@
+﻿public class ErrorMessages
+{
+	public const string ERR_SEQUENCE_OUT_OF_BOUNDS = "Sequence index out of bounds. The recipe cannot be created because sequence cannot be defined.";
+	public const string ERR_MISSING_RECIPE         = "Recipe is does not have a set value. Please check the recipe data.";
+	public const string ERR_INVALID_ORDER_TYPE     = "Order type is missing. Please check your order pool data.";
+	public const string ERR_ORDER_TYPE_EMPTY       = "Order type has no entry. Please check your order pool data.";
+}
