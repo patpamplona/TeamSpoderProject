8f091d1e47e37aacb15caadeba9697461207c4cb
Patrick Karlo Pamplona <patrick.pam@pocketplaylab.com>
2015-05-21 09:57:07 +0800

    order data and management

---
+++ Project/ShakeEm/Assets/Game/Scripts/Data/FoodOrder.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+public class FoodOrder : AOrder
+{
+	public override void StartRecipe ()
+	{
+		sequenceIndex = 0;
+	}
+
+	public override bool AddRecipe (string ingredient)
+	{
+		if(recipe == null || recipe.Length == 0)
+		{
+			throw new System.Exception(ErrorMessages.ERR_MISSING_RECIPE);
+		}
+
+		if(sequenceIndex < 0 || sequenceIndex >= recipe.Length)
+		{
+			throw new System.Exception(ErrorMessages.ERR_SEQUENCE_OUT_OF_BOUNDS);
+		}
+
+		//Player got the ingredient in the right order
+		if(recipe[sequenceIndex] == ingredient)
+		{
+			sequenceIndex++;
+			return true;
+		}
+
+		//Player failed to add the right ingredient
+		return false;
+	}
+}
---
+++ Project/ShakeEm/Assets/Game/Scripts/Data/OrderManager.cs
@@ -0,0 +1,86 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class OrderManager : MonoBehaviour 
+{
+	#region Singleton and Instance management
+	private static OrderManager _instance = null;
+	public static OrderManager Instance
+	{
+		get
+		{
+			return _instance;
+		}
+	}
+
+	void Awake()
+	{
+		_instance = this;
+		AwakeOrderManager();
+	}
+
+	void OnDestroy()
+	{
+		_instance = null;
+		DestroyOrderManager();
+	}
+	#endregion
+	#region Instance Fields and Properties
+	//Only use this for inspector setup
+	[SerializeField] private AOrder[] arrOrders;
+
+	private Dictionary<OrderClassification, List<AOrder>> orderDictionary;
+	#endregion
+
+	#region Setup
+	private void AwakeOrderManager()
+	{
+		orderDictionary = new Dictionary<OrderClassification, List<AOrder>>();
+
+		for(int o = 0; o < arrOrders.Length; o++)
+		{
+			AOrder order = arrOrders[o];
+			if(!orderDictionary.ContainsKey(order.OrderType))
+			{
+				orderDictionary.Add(order.OrderType, new List<AOrder>());
+			}
+
+			orderDictionary[order.OrderType].Add(order);
+		}
+
+		arrOrders = null;
+	}
+
+	private void DestroyOrderManager()
+	{
+		foreach(KeyValuePair<OrderClassification, List<AOrder>> pair in orderDictionary)
+		{
+			pair.Value.Clear();
+		}
+
+		orderDictionary.Clear();
+		orderDictionary = null;
+	}
+	#endregion
+
+	#region Order Manager methods
+	public AOrder GetOrder(OrderClassification orderType)
+	{
+		if(!orderDictionary.ContainsKey(orderType))
+		{
+			throw new System.Exception(ErrorMessages.ERR_INVALID_ORDER_TYPE);
+		}
+
+		List<AOrder> newOrder = orderDictionary[orderType];
+
+		if(newOrder.Count == 0)
+		{
+			throw new System.Exception(ErrorMessages.ERR_ORDER_TYPE_EMPTY);
+		}
+
+		int index = Random.Range(0, newOrder.Count);
+		return newOrder[index];
+	}
+	#endregion
+}
