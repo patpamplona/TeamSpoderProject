023775ee3f6cf316d5d608d6ee0675224bb4b035
Patrick Karlo Pamplona <patrick.pam@pocketplaylab.com>
2015-06-06 21:53:21 +0800

    Adjustable Grid Layout for more organized and flexible order bubble

---
+++ Project/ShakeEm/Assets/Game/Scripts/UI/AdjustableGrid.cs
@@ -0,0 +1,81 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+
+public class AdjustableGrid : MonoBehaviour 
+{
+	[SerializeField] RectTransform   rect;
+	[SerializeField] ScrollRect      scroll;
+	[SerializeField] GridLayoutGroup grid;
+
+	[SerializeField] int instanceMargin = 2;
+	[SerializeField] GameObject imagePrefab;
+
+	[SerializeField] List<Image> images;
+
+	public void FocusOnChild(int child)
+	{
+		child = Mathf.Clamp(child, 0, scroll.content.childCount);
+		images.ForEach(img => ScaleOnFocus(img, child));
+		float normalized = ((float)child + ((float)child - 1)) / ((float)scroll.content.childCount + ((scroll.content.childCount - 1) * (grid.spacing.x / grid.cellSize.x)));
+		scroll.horizontalNormalizedPosition = normalized;
+	}
+
+	private void ScaleOnFocus(Image img, int child)
+	{
+		bool focus = images[child] == img;
+		img.transform.localScale = focus ? new Vector3(1.5f, 1.5f, 1.0f) : Vector3.one;
+		Color c = img.color;
+		c.a = focus ? 1.0f : 0.5f;
+		img.color = c;
+	}
+
+	public void SetGrid(List<string> spriteIds)
+	{
+		if(images == null)
+		{
+			this.images = new List<Image>();
+		}
+
+		for(int i = 0; i < spriteIds.Count || i < this.images.Count; i++)
+		{
+			if(i >= spriteIds.Count)
+			{
+				if(i < this.images.Count)
+				{
+					this.images[i].gameObject.SetActive(false);
+				}
+			}
+			else
+			{
+				Image img = null;
+
+				if(i >= images.Count)
+				{
+					GameObject go = new GameObject();
+					go.transform.SetParent(transform);
+					go.transform.localScale = Vector3.one;
+					DisplayUtils.SetLayer(go, gameObject.layer);
+					img = go.AddComponent<Image>();
+					images.Add(img);
+				}
+
+				img = images[i];
+				img.preserveAspect = true;
+				img.gameObject.name = spriteIds[i];
+				img.gameObject.SetActive(true);
+				img.sprite = SpritePool.Instance.GetSprite(spriteIds[i]);
+			}
+		}
+
+		int objCount  = spriteIds.Count + instanceMargin;
+		float _width  = objCount * grid.cellSize.x;
+		float _margin = (objCount - 1) * grid.spacing.x;
+
+		Vector2 _size = rect.sizeDelta;
+		_size.x = _width + _margin;
+
+		rect.sizeDelta = _size;
+	}
+}
---
+++ Project/ShakeEm/Assets/Game/Scripts/UI/DisplayUtils.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DisplayUtils
+{
+	public static void SetLayer(GameObject go, int layer)
+	{
+		go.layer = layer;
+		foreach(Transform child in go.transform)
+		{
+			child.gameObject.layer = layer;
+		}
+	}
+}
