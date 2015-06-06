40ff9b6b2af074f2e7d820818f8acbb6a5c46b25
Patrick Karlo Pamplona <patrick.pam@pocketplaylab.com>
2015-06-06 22:42:30 +0800

    added score management (computation and display)

---
+++ Project/ShakeEm/Assets/Game/Scripts/Data/ScoreManager.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ScoreManager : MonoBehaviour 
+{
+	private static ScoreManager _instance = null;
+	public static ScoreManager Instance
+	{
+		get
+		{
+			return _instance;
+		}
+	}
+
+	[SerializeField] private Text scoreText;
+
+	private int score = 0;
+
+	void Awake()
+	{
+		_instance = this;
+	}
+
+	void OnDestroy()
+	{
+		_instance = null;
+	}
+
+	// Use this for initialization
+	void Start () 
+	{
+		scoreText.text = "0";
+	}
+
+	public void AddScore(int moreScore)
+	{
+		Debug.LogError("ADDING SCORE : " + moreScore);
+
+		score += moreScore;
+		score = Mathf.Clamp(score, 0, score);
+
+		scoreText.text = DisplayUtils.ConvertToThousands(score);
+	}
+}
