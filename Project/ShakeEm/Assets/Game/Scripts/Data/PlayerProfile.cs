fc1fd35b95d1189b84c4bc6803ddd3f3edcb0242
Kristian Mitra <kristian.m@pocketplaylab.com>
2015-06-07 22:19:18 +0800

    Added PlayerProfile class to keep track of the player's state throughout the game.

---
+++ Project/ShakeEm/Assets/Game/Scripts/Data/PlayerProfile.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum PlayerType {
+
+	PlayerHost,
+	PlayerClient,
+	None
+}
+
+
+public class PlayerProfile {
+	
+	private static PlayerProfile instance = null;
+	public static PlayerProfile GetInstance() {
+
+		if (instance == null) {
+			instance = new PlayerProfile();
+		}
+
+		return instance;
+	}
+
+	public string playerName = "defaultPlayerName";
+	public string storeName = "defaultStoreName";
+	public PlayerType playerType = PlayerType.None;
+
+	public bool isConnected = false;
+
+	private PlayerProfile() {
+
+		// Constructor
+	}
+}
