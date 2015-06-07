07646d958180c9554c12f824def7e8f42bc0b5d9
Kristian Mitra <kristian.m@pocketplaylab.com>
2015-06-08 07:45:29 +0800

    Added Query and Connect function to Available Stores

---
+++ Project/ShakeEm/Assets/Game/Scripts/UI/GameLobby/HostItem.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class HostItem : MonoBehaviour {
+
+	[SerializeField] private Text buttonText = null;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	public Button GetButton() {
+
+		Button tmpButton = this.gameObject.GetComponent<Button>();
+		return tmpButton;
+	}
+
+	public void SetHostName(string hostName) {
+
+		buttonText.text = hostName;
+	}
+}
--- Project/ShakeEm/Assets/Game/Scripts/UI/GameLobby/PanelAvailableStores.cs
+++ Project/ShakeEm/Assets/Game/Scripts/UI/GameLobby/PanelAvailableStores.cs
@@ -1,21 +1,35 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
 using System.Collections;
+using System.Collections.Generic;
 
 public class PanelAvailableStores : BasePanelLobby {
 
 	[SerializeField] private GameObject panelQueryingServer = null;
-	[SerializeField] private GameObject panelHostList = null;
-
+	[SerializeField] private GameObject panelQueryComplete = null;
+	[SerializeField] private GameObject panelHostsList = null;
+	[SerializeField] private GameObject HostItemPrefab = null;
+	
 	private HostData[] hostsList;
+	private List<HostItem> hostItemList;
 
 	public override void Start () {
 		//base.Start ();
+
+		hostItemList = new List<HostItem>();
+
+		ShowQueryingServer();
+		this.StartCoroutine(this.RefreshHostsList());
 	}
 
 	public override void OnShow ()
 	{
 		base.OnShow ();
+	}
+
+	public override void OnShowComplete() {
+
+		RefreshHostsList();
 	}
 
 	public void ChangeToNextScreen() {
@@ -25,9 +39,44 @@ public class PanelAvailableStores : BasePanelLobby {
 
 	private void PopulateAvailableHosts() {
 
+		ClearHostItemsList();
+
 		// Generate different buttons,
 		// Add listeners depending on index
 
+		int maxCount = hostsList.Length;
+		for(int i = 0; i < maxCount; i++) {
+
+			HostData hostData = hostsList[i];
+
+			Debug.Log("Host #" + (i + 1) + ": " + hostData.gameName);
+			GameObject item = Instantiate(HostItemPrefab) as GameObject;
+			item.transform.SetParent(panelHostsList.transform);
+			item.transform.localPosition = Vector3.zero;
+			item.transform.localScale = Vector3.one;
+
+			HostItem hostItem = item.GetComponent<HostItem>();
+			hostItem.SetHostName(hostData.gameName);
+			hostItem.GetButton().onClick.AddListener(() => {
+				OnConnectToHostClick(hostData);
+			});
+
+			hostItemList.Add(hostItem);
+		}
+	}
+
+	private void ClearHostItemsList() {
+
+		while(hostItemList.Count > 0) {
+
+			int maxCount = hostItemList.Count;
+			HostItem item = hostItemList[maxCount - 1];
+			hostItemList.Remove(item);
+
+			Destroy(item);
+		}
+
+		hostItemList.Clear();
 	}
 
 	#region Query Server for Hosts List
@@ -35,12 +84,12 @@ public class PanelAvailableStores : BasePanelLobby {
 	private void ShowQueryingServer() {
 
 		panelQueryingServer.SetActive (true);
-		panelHostList.SetActive (false);
+		panelQueryComplete.SetActive (false);
 	}
 
 	private void ShowHostsList() {
 
-		panelHostList.SetActive (true);
+		panelQueryComplete.SetActive (true);
 		panelQueryingServer.SetActive (false);
 	}
 
@@ -52,7 +101,7 @@ public class PanelAvailableStores : BasePanelLobby {
 		float timeStart = Time.time;
 		float timeEnd = timeStart + GameConstants.REQUEST_REFRESH_LENGTH;
 
-		while (timeEnd < Time.time) {
+		while (Time.time < timeEnd) {
 
 			hostsList = MasterServer.PollHostList();
 			yield return new WaitForEndOfFrame();
@@ -64,6 +113,7 @@ public class PanelAvailableStores : BasePanelLobby {
 		} else {
 
 			// No servers found.
+			Debug.Log ("No Servers Found!");
 		}
 	}
 
@@ -72,19 +122,16 @@ public class PanelAvailableStores : BasePanelLobby {
 
 	#region Select and Connect to Server
 
-	public void OnConnectToHostClick(int hostIdx) {
+	public void OnConnectToHostClick(HostData host) {
 
-		HostData host = hostsList [hostIdx];
-
-		if (host == null)
-			return;
-
-		Network.Connect (host);
+		Debug.Log("Connecting to Host: " + host.gameName);
+		Network.Connect(host);
 	}
 	
 	void OnConnectedToServer() {
 
 		Debug.Log ("Connected to Server!");
+		PlayerProfile.GetInstance().playerType = PlayerType.PlayerClient;
 		ChangeToNextScreen();
 	}
 
