ccfc23170619ca5425650bba9909f5649b5ab27d
Kristian Mitra <kristian.m@pocketplaylab.com>
2015-06-07 22:18:52 +0800

    Adding GameConstants class to keep all const variables used across all scenes/classes.

---
+++ Project/ShakeEm/Assets/Game/Scripts/Data/GameConstants.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameConstants {
+
+	public const int SCREEN_WIDTH = 800;
+	public const int SCREEN_HEIGHT = 1280;
+
+	// Network Connections
+	public const string UGID = "com.teamspoder.shakem.masterserver.name"; // Unique Game ID
+	public const int MAX_REQUEST = 3;
+	public const int PORT = 1234;
+}
