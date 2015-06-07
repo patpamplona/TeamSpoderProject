9f1ae9c35fa630074587c94d969e76c900890a93
Kristian Mitra <kristian.m@pocketplaylab.com>
2015-06-08 07:44:51 +0800

    Added OnComplete events for Show and Hide

--- Project/ShakeEm/Assets/Game/Scripts/UI/GameLobby/BasePanelLobby.cs
+++ Project/ShakeEm/Assets/Game/Scripts/UI/GameLobby/BasePanelLobby.cs
@@ -25,15 +25,27 @@ public class BasePanelLobby : MonoBehaviour {
 		// override to capture on hide event.
 	}
 
+	public virtual void OnShowComplete() {
+
+		// override to capture event when animation completes.
+	}
+
+	public virtual void OnHideComplete() {
+
+		// override to capture event when animation completes.
+	}
+
 	public void ShowPanel() {
 
 		this.OnShow();
 		rectTrans.localPosition = Vector3.zero;
+		this.OnShowComplete();
 	}
 
 	public void HidePanel() {
 
-		rectTrans.localPosition = new Vector3 (0, GameConstants.SCREEN_HEIGHT * -1, 0);
 		this.OnHide();
+		rectTrans.localPosition = new Vector3 (0, GameConstants.SCREEN_HEIGHT * -1, 0);
+		this.OnHideComplete();
 	}
 }
