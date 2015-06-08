619a26cc4b0c708202720f06c3bbecc80ff117a3
Patrick Karlo Pamplona <patrick.pam@pocketplaylab.com>
2015-06-08 20:29:57 +0800

    added distribution of action ingredients and spreads ingredients to players with only one correct ingredient

--- Project/ShakeEm/Assets/Game/Scripts/Gameplay/IngredientHandler.cs
+++ Project/ShakeEm/Assets/Game/Scripts/Gameplay/IngredientHandler.cs
@@ -52,6 +52,29 @@ public class IngredientHandler : MonoBehaviour
 		ingredientIds = null;
 	}
 
+	public void SpreadActionIngredients()
+	{
+		cup.gameObject.SetActive(false);
+		blender.gameObject.SetActive(false);
+		straw.gameObject.SetActive(false);
+
+		switch(NetworkManager.Instance.PlayerIndex)
+		{
+			case 0:
+				cup.gameObject.SetActive(true);
+				straw.gameObject.SetActive(NetworkManager.Instance.MaxPlayers == 1);
+				break;
+
+			case 1: 
+				blender.gameObject.SetActive(true);
+				break;
+
+			case 2:
+				straw.gameObject.SetActive(true);
+				break;
+		}
+	}
+
 	public void GenerateIngredients()
 	{
 		if(NetworkManager.Instance.IsConnected)
@@ -130,19 +153,80 @@ public class IngredientHandler : MonoBehaviour
 
 	public void SpreadIngredientsToPlayers()
 	{
-		GenerateIngredientsTable();
+		if(NetworkManager.Instance.IsServer)
+		{
+			string[] ids = new string[Network.maxConnections + 1];
+
+			int start = 1;
+
+			string validId = OrderManager.Instance.CurrentIngredient;
+
+			if(validId == cup.IngredientID || validId == blender.IngredientID || validId == straw.IngredientID)
+			{
+				start = 0;
+			}
+			else
+			{
+				ids[0] = validId;
+			}
+
+
+			for(int i = start; i <= Network.maxConnections; i++)
+			{
+				int index = Random.Range(0, ingredientIds.Length);
+
+				while(OrderManager.Instance.CurrentRecipe.IsPartOfRecipe(ingredientIds[index]))
+				{
+					index = Random.Range(0, ingredientIds.Length);
+				}
+
+				ids[i] = ingredientIds[index];
+			}
+
+			ids = ShuffleIngredients(ids);
+
+			string mergedIds = ids[0];
+
+			for(int i = 1; i < ids.Length; i++)
+			{
+				mergedIds += "|" + ids[i];
+			}
+
+			RPCHandler.Instance.CallRPCSpreadIngredients(mergedIds);
+		}
+	}
+
+	public void ReceiveAssignedIngredient(string ingredient)
+	{
+		string ingId = ingredient.Split('|')[NetworkManager.Instance.PlayerIndex];
+
+		if(ingredientsInBoard != null)
+		{
+			foreach(Ingredient iib in ingredientsInBoard)
+			{
+				IngredientPool.Instance.ReturnBorrowed(iib);
+				iib.gameObject.SetActive(false);
+			}
+		}
+
+		ingredientsInBoard  = new Ingredient[1];
+
+		Ingredient ing = IngredientPool.Instance.Borrow();
+		
+		ing.transform.SetParent(woodenTable.transform);
+		ing.transform.localScale = Vector3.one;
+		DisplayUtils.SetLayer(ing.gameObject, woodenTable.layer);
+
+		ing.SetIngredient(ingId);
+		ing.gameObject.SetActive(true);
+		
+		ingredientsInBoard[0] = ing;
 	}
 
 	public string GetRandomIngredientSinglePlayer()
 	{
 		int index = Random.Range(0, ingredientIds.Length);
 		return ingredientIds[index];
-	}
-
-	public string GetRandomIngredientMultiPlayer()
-	{
-		//Send only one valid ingredient per player
-		return "";
 	}
 
 	public void Reset()
